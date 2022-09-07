2c156ba3f4b04fb260e0a16a9cc1e697d2c0cead
Brian McCune <brian.mccune@gmail.com>
2022-09-07 10:47:27 -0700

    removed namespaces

--- src/components/brand-logo.js
+++ src/components/brand-logo.js
@@ -7,10 +7,6 @@ export default function GatsbyContentfulLogo() {
       height="24"
       viewBox="0 0 1800 300" 
       version="1.1" 
-      xmlns="http://www.w3.org/2000/svg"
-      xmlns:xlink="http://www.w3.org/1999/xlink"
-      xml:space="preserve"
-      xmlns:serif="http://www.serif.com/"
       style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;"
      >
       <g transform="matrix(1,0,0,1,-354.45,-879.383)">
